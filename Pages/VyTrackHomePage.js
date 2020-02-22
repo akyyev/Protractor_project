3409e3f1bdb117f77bf9fa3a68371ae0d5aa18d6
akyyevtm <akyyevtm@gmail.com>
2020-02-21 23:35:33 -0500

    assertion and then method

--- Pages/VyTrackHomePage.js
+++ Pages/VyTrackHomePage.js
@@ -1,6 +1,7 @@
 var Elements = function(){
     this.Fleet =  element(By.xpath("(//span[contains(text(), 'Fleet')])[1]"));
     this.Vehicles = element(by.xpath("//span[contains(text(), 'Vehicles')]"));
+    this.dashboard = element(by.xpath("//a/span[contains(text(), '   Dashboards')]"));
 };
 
 module.exports = new Elements();
--- Specs/Spec7.js
+++ Specs/Spec7.js
@@ -1,11 +1,15 @@
 var actions = browser.actions();
 var VyTrackHomePage = require("../Pages/VyTrackHomePage.js");
-
+var assert = require('assert');
 
 describe('Test group 7', () => {
     it('Move mouse hover to Fleet', () => {
         actions.mouseMove(VyTrackHomePage.Fleet).perform();
         browser.sleep(3000);
+        var dashboardsText = VyTrackHomePage.dashboard.getText().then(function(text){
+            console.log(text);
+        })
+        assert(dashboardsText, 'Dashboards', 'Actual text did not match expected text');
     });
 
     it('Click Vehicles', () => {
@@ -13,3 +17,4 @@ describe('Test group 7', () => {
         browser.sleep(3000);
     });
 });
+
